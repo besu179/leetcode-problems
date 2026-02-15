1a665b8c89204e2a471bf8f67185e20555b95ab5
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-02-15 18:48:42 +0300

    Time: 0 ms (100%), Space: 12.4 MB (48.84%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def escapeGhosts(self, ghosts, target):
+        """
+        :type ghosts: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        target_dist = abs(target[0]) + abs(target[1])
+        for r, c in ghosts:
+            ghost_target = abs(target[0] - r) + abs(target[1] - c)
+            if ghost_target <= target_dist:
+                return False
+        return True
