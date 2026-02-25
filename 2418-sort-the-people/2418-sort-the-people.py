387da983a02d4d09df2fff356cf073717e6d32a5
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-02-25 17:00:09 +0300

    Time: 1144 ms (5.07%), Space: 12.8 MB (13.26%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -7,8 +7,12 @@ class Solution(object):
         """
         n = len(names)
         for i in range(n -1):
+            swaps = 0
             for j in range(n - i - 1):
                 if heights[j] < heights[j + 1]:
                     heights[j], heights[j + 1] = heights[j + 1], heights[j]
                     names[j], names[j + 1] = names[j + 1], names[j]
+                    swaps += 1
+            if swaps == 0:
+                return names
         return names
