aa99e81405f36570aef1588a207aa8919ba617e7
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-08 15:40:42 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -5,13 +5,12 @@ class Solution(object):
         :rtype: None Do not return anything, modify arr in-place instead.
         """
         i = 0
-        while i < len(arr) - 1: 
+        while i < len(arr) - 1:
             if arr[i] == 0:
                 j = len(arr) - 1
                 while j > i + 1:
                     arr[j] = arr[j - 1]
                     j -= 1
-                
                 arr[i + 1] = 0
                 i += 2
             else:
