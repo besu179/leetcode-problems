0b5b5465f4d11823918c1157db35fb7c6c508cdb
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-13 09:49:06 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,11 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        k = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]: 
-                nums[k] = nums[i]
-                k += 1
-        return k
-
-# to solve indexoutofrangebound compare i with i - 1. 
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l]= nums[r]
+                l += 1
+        return l
