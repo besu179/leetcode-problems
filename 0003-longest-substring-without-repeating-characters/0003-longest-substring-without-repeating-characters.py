46a5d42b00e34a172f7441fa20df1012b7723720
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-13 20:58:43 +0300

    Time: 26 ms (47.33%), Space: 13.3 MB (20.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,18 +1,16 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        char_map = {}
-        max_length = 0
-        left = 0
-        
-        for right in range(len(s)):
-            # If character is found in the current window, move left pointer
-            if s[right] in char_map and char_map[s[right]] >= left:
-                left = char_map[s[right]] + 1
-            
-            # Update the last seen index of the character
-            char_map[s[right]] = right
-            
-            # Calculate and update the maximum length
-            max_length = max(max_length, right - left + 1)
-            
-        return max_length
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = ans = 0
+        seen = set()
+
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            ans = max(ans, r - l + 1)
+        return ans
