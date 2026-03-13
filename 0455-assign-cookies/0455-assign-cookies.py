46bc484d6209ace22af2ba2bf07336fb9838b888
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-13 10:32:50 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -7,14 +7,17 @@ class Solution(object):
         """
         g.sort()
         s.sort()
-        i = j = 0
-        ans = 0
+
+        i = 0 
+        j = 0 
+        count = 0
 
         while i < len(g) and j < len(s):
             if s[j] >= g[i]:
-                j += 1
+                count += 1
                 i += 1
-                ans += 1
+                j += 1
             else:
                 j += 1
-        return ans
+
+        return count
