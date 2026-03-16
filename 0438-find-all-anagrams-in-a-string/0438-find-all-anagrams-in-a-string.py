5edf9b0aa1c66505f022c080f83c5a1d8066e343
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-16 13:06:20 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -4,23 +4,17 @@ class Solution(object):
         :type s: str
         :type p: str
         :rtype: List[int]
-        
-        phm = counter(p)
-
-        l = 0
+        """
+        phm = Counter(p)
+        thm = Counter(s[:len(p) - 1])
         ans = []
 
-        for r in range(len(s)):"""
-        result = []
-        target_counts = Counter(p)
-        window_counts = Counter(s[:len(p) - 1])
+        for r in range(len(p) - 1, len(s)):
+            thm[s[r]] += 1
+            if thm == phm:
+                ans.append(r - len(p) + 1)
+            thm[s[r - len(p) + 1]] -= 1
+            if thm[s[r - len(p) + 1]] == 0:
+                del thm[s[r - len(p) + 1]]
 
-        for i in range(len(p) - 1, len(s)):
-            curr_char = s[i]
-            window_counts[curr_char] += 1
-            if window_counts == target_counts:
-                result.append(i - len(p) + 1)
-            window_counts[s[i - len(p) + 1]] -= 1
-            if window_counts[s[i - len(p) + 1]] == 0:
-                del window_counts[s[i - len(p) + 1]]
-        return result
+        return ans
