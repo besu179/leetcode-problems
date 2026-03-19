20d58200cfd3b6b83a4f5df42204f6df392b69e8
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-19 21:19:36 +0300

    Time: 53 ms (63.31%), Space: 31.3 MB (72.41%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        remainder_map = {0: -1}
+        curr_sum = 0
+        
+        for i, num in enumerate(nums):
+            curr_sum += num
+            remainder = curr_sum % k
+            
+            if remainder in remainder_map:
+                if i - remainder_map[remainder] > 1:
+                    return True
+            else:
+                remainder_map[remainder] = i
+                
+        return False
