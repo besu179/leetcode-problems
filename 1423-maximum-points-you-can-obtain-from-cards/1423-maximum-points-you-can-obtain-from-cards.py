504c028a6c3aac094373fffeddbd1dadfb878d72
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-20 21:55:15 +0300

    Time: 21 ms (82.01%), Space: 19.4 MB (27.69%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -4,25 +4,16 @@ class Solution(object):
         :type cardPoints: List[int]
         :type k: int
         :rtype: int
-        """
-        n = len(cardPoints)
-        
-        total_sum = sum(cardPoints)
-        
-        if k == n:
-            return total_sum
-            
-        window_size = n - k
-        
-        current_window_sum = sum(cardPoints[0:window_size])
-        min_window_sum = current_window_sum
+        """        
+        s = sum(cardPoints)
+                    
+        cur_sum = sum(cardPoints[0:len(cardPoints) - k])
+        min_sum = cur_sum
         
         l = 0
-        for r in range(window_size, n):
-            current_window_sum += cardPoints[r] - cardPoints[l]
-            
-            min_window_sum = min(min_window_sum, current_window_sum)
-            
+        for r in range( len(cardPoints) - k, len(cardPoints)):
+            cur_sum += cardPoints[r] - cardPoints[l]
+            min_sum = min(min_sum, cur_sum)
             l += 1
             
-        return total_sum - min_window_sum
+        return s - min_sum
