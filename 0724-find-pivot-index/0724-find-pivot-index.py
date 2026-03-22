54ee463ed1a14e26d917a4f054338bc2adfafa6d
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-22 10:21:37 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,18 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        s_left = [nums[0]]
-        s_right = [0] * (n + 1)
-
-        for i in range(len(nums)- 1, -1, -1):
-            s_right[i] += s_right[i + 1] + nums[i]
-
-        for i in range(1, len(nums)):
-            s_left.append(nums[i] + s_left[i - 1])
-
-        for i in range(n):
-            if s_left[i] == s_right[i]:
+        total = sum(nums)
+        left_sum = 0
+        
+        for i in range(len(nums)):
+            if left_sum == total - left_sum - nums[i]:
                 return i
-                
+            left_sum += nums[i]
+        
         return -1
