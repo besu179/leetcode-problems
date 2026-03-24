fef6d1bb49cc2e80c5a87ad0714c23a1d740dfc0
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-03-24 22:26:32 +0300

    Time: 43 ms (98.34%), Space: 31.1 MB (85.06%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -5,17 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        remainder_map = {0: -1}
+        hm = {0: -1}
         curr_sum = 0
         
         for i, num in enumerate(nums):
             curr_sum += num
             remainder = curr_sum % k
             
-            if remainder in remainder_map:
-                if i - remainder_map[remainder] > 1:
+            if remainder in hm:
+                if i - hm[remainder] > 1:
                     return True
             else:
-                remainder_map[remainder] = i
+                hm[remainder] = i
                 
         return False
