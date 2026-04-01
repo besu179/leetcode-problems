16ca457b321e579bff741f5baddbe5d3d0614c90
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-04-01 23:39:26 +0300

    Time: 88 ms (30.23%), Space: 12.5 MB (98.15%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        def isPalindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
         left, right = 0, len(s) - 1
         while left < right:
             if s[left] != s[right]:
-                one, two = s[left:right], s[left + 1:right + 1]
-                return one == one[::-1] or two == two[::-1]
-            left, right = left + 1, right - 1
+                return isPalindrome(left + 1, right) or isPalindrome(left, right - 1)
+            left += 1
+            right -= 1
         return True
