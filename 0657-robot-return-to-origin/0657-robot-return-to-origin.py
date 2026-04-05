d5a5681fd0c8ef5bc1d8e0551f7186d22cfdd578
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-04-05 20:25:06 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -4,16 +4,4 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        x = 0
-        y = 0
-
-        for move in moves:
-            if move == "U":
-                y += 1
-            elif move == "D":
-                y -= 1
-            elif move == "R":
-                x += 1
-            else:
-                x -= 1
-        return x == y == 0
+        return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")
