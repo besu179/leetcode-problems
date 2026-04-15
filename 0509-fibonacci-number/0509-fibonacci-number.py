2b602767a7747157ef757804e8585561dfe84176
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-04-15 10:39:13 +0300

    Time: 661 ms (18.62%), Space: 12.4 MB (26.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 1:
-            return n
-
+        if n == 1:
+            return 1
+        elif n == 0:
+            return 0
         else:
             return self.fib(n - 1) + self.fib(n - 2)
