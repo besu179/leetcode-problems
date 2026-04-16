1deaf6ffc209f4526da3306d6fef2deb90c53fa0
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-04-16 22:11:00 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,20 +1,14 @@
 class Solution(object):
-    def recursion(self, n, k):
-        if n == 1:
-            return 0
-
-        total_elements = 2 ** (n - 1)
-        half_elements = total_elements // 2
-
-        if k > half_elements:
-            return 1 - self.recursion(n, k - half_elements)
-
-        return self.recursion(n - 1, k)
-
     def kthGrammar(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: int
         """
-        return self.recursion(n, k)
+        if n == 1:
+            return 0
+
+        if k % 2:
+            return self.kthGrammar(n - 1, (k + 1 )// 2)
+        else:
+            return 1 - self.kthGrammar(n - 1, (k + 1 )// 2)
