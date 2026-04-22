d69b8dc2d9357da7c0b338481ab7474aea96476b
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-04-22 13:06:50 +0300

    Time: 0 ms (100%), Space: 13.1 MB (43.82%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,17 +5,36 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        front = 0
-        back = len(nums) - 1
-        ans = [-1, -1]
-        
-        while front < len(nums):
-            if ans[0] == -1 and nums[front] == target:
-                ans[0] = front
-            if ans[1] == -1 and nums[back] == target:
-                ans[1] = back
-            if ans[0] != -1 and ans[1] != -1:
-                break
-            front += 1
-            back -= 1
-        return ans
+        def findFirst():
+            idx = -1
+            l, r = 0, len(nums) - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums[mid] == target:
+                    idx = mid
+                    r = mid - 1
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return idx
+
+        def findLast():
+            idx = -1
+            l, r = 0, len(nums) - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums[mid] == target:
+                    idx = mid
+                    l = mid + 1
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return idx
+
+        return [findFirst(), findLast()]
