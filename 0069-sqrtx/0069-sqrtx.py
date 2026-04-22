e598d9ed1172fda7a37c5ce7c04a3744e9473131
Besufkad Yalew <140785596+besu179@users.noreply.github.com>
2026-04-22 13:24:18 +0300

    Time: 4 ms (43.8%), Space: 12.5 MB (23.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        return int(sqrt(x))
+        if x < 2:
+            return x
+        
+        l, r = 1, x // 2
+        ans = 0
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
